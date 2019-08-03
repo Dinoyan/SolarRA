18cb552ab9046fe15f916342e157b04f36f4128f
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2019-08-03 16:35:22 -0400

    Implemented get water and air sensor code

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,19 @@
 #include <SPI.h>
 #include <SD.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define WATER_TEMP 5
 const int chipSelect = 4;
+
+OneWire oneWire(WATER_TEMP);
+// BUS
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  
   
   SD.begin(chipSelect);
   File logFile = SD.open("logger.txt", FILE_WRITE);
@@ -21,4 +30,18 @@ void loop() {
 
 void logData(String data) {
   logFile.write(data);
+}
+
+float getWaterTemp() {
+  float temp;
+  sensors.requestTemperatures();
+  temp = sensors.getTempCByIndex(0);
+  return temp;
+}
+
+float getAirTemp() {
+  float temp;
+  sensors.requestTemperatures();
+  temp = sensors.getTempCByIndex(1);
+  return temp;
 }
