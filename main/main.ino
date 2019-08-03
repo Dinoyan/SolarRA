91652d0e214f63a39fc7cd01d526f49ac091a98a
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2019-08-03 16:46:15 -0400

    Revert "Added to the Main.ino"
    This reverts commit 23cc6087dafb26cac0ee9a4577f0f4f10ce0f91b.

--- main/main.ino
+++ main/main.ino
@@ -1,136 +1,47 @@
 #include <SPI.h>
 #include <SD.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define WATER_TEMP 5
 const int chipSelect = 4;
-OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
-const int Min_Temp = 5; //Arbitrary value in which the heater is going to turn ON
-const int Max_Temp = 8; //Arbitrary value in which the heater is going to turn OFF
+
+OneWire oneWire(WATER_TEMP);
+// BUS
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  
   SD.begin(chipSelect);
   File logFile = SD.open("logger.txt", FILE_WRITE);
 
   if (!logFile) {
     Serial.println("SD Failed");
   }
-    //start serial connection
-  Serial.begin(9600);
-  pinMode(6, OUTPUT); //Configure PIN 6 as Trigger for the HEATER
-  pinMode(7, OUTPUT); //Configure PIN 7 as Trigger for the MOTOR
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius;
 
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
-
-  Serial.print("ROM =");
-  for ( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid!");
-    return;
-  }
-  Serial.println();
-
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  if (celsius < Min_Temp) {
-    Turn_Heater_ON();
-  }
-  else if (celsius > Max_Temp) {
-    Turn_Heater_OFF();
-  }
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
 }
 
 void logData(String data) {
   logFile.write(data);
 }
-void Turn_Heater_ON() {
-  digitalWrite(6, HIGH); //Trigger the signal for HEATER ON
-  digitalWrite(7, LOW); //Trigger the signal for MOTOR OFF
+
+float getWaterTemp() {
+  float temp;
+  sensors.requestTemperatures();
+  temp = sensors.getTempCByIndex(0);
+  return temp;
 }
-void Turn_Heater_OFF() {
-  digitalWrite(6, LOW); //Trigger the signal for HEATER OFF
-  digitalWrite(7, HIGH); //Trigger the signal for MOTOR ON
+
+float getAirTemp() {
+  float temp;
+  sensors.requestTemperatures();
+  temp = sensors.getTempCByIndex(1);
+  return temp;
 }
